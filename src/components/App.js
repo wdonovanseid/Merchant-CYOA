c2bea7e1e4bb94280afde1a1d2f6221b94d2bf54
William Donovan-Seid <wdstwo@live.com>
2020-12-16 09:18:38 -0800

    add starting idea App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,56 @@
 import React from "react";
+import PropTypes from "prop-types";
+import { connect } from 'react-redux';
+import * as a from "./../actions/index.js";
+import NewCharacterForm from "./NewCharacterForm";
 
-function App() {
-  return (
-    <React.Fragment>
+class App extends React.Component {
 
-    </React.Fragment>
-  );
+  handleCreatingNewCharacter = (newPlayerCharacter) => {
+    this.props.dispatch(a.createNewPlayerCharacter(newPlayerCharacter));
+    this.props.dispatch(a.startGame);
+  }
+
+  handleReturnToStart = () => {
+    this.props.dispatch(a.showSplashScreen);
+  }
+
+  render() {
+    let currentlyVisibleStartingScreen = null;
+    if (this.props.initialScreenToShow === "NEW_CHAR_FORM") {
+      currentlyVisibleStartingScreen =
+      <NewCharacterForm 
+        onNewCharacterCreation={this.handleCreatingNewCharacter}
+        returnToStart={this.handleReturnToStart}
+      />
+    } else if (this.props.initialScreenToShow === "START_GAME") {
+      currentlyVisibleStartingScreen =
+      <GameControl 
+        playerCharacter={this.props.playerCharacter}
+        endGame={this.handleReturnToStart}
+      />
+    } else {
+      currentlyVisibleStartingScreen =
+      <StartScreen />
+    }
+    return (
+      <React.Fragment>
+        {currentlyVisibleStartingScreen}
+      </React.Fragment>
+    );
+  }
 }
+
+App.propTypes = {
+  initialScreenToShow: PropTypes.string,
+}
+
+const mapStateToProps = state => {
+  return {
+    initialScreenToShow: state.initialScreenToShow
+  }
+}
+
+App = connect(mapStateToProps)(App);
 
 export default App;
